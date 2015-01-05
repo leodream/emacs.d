292adfa1f9710dd3deb44b3cf2ea92548eec6d34
Leo He <leodream2008@gmail.com>
2015-01-05 11:58:30 +0800

    Also search DONE items in agenda view.

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -109,13 +109,9 @@
       '(
         ("u" "Timeline for today and tasks that under going"
          ((agenda "" )
-          (todo "PROJECT")
           (todo "BEGINED")
           (todo "READY")
-          (todo "WAITING")
-          (todo "TOBREAKDOWN")
-          (todo "SOMEDAY")
-          (todo "TODO"))
+          (todo "WAITING"))
          ((org-agenda-ndays 1)
           (org-agenda-show-log t)
           (org-agenda-clockreport-mode t)
@@ -136,7 +132,9 @@
           (tags-todo "-SCHEDULED={.+}/+WAITING")
           (tags-todo "-SCHEDULED={.+}/+TOBREAKDOWN")
           (tags-todo "-SCHEDULED={.+}/+SOMEDAY")
-          (tags-todo "-SCHEDULED={.+}/+TODO")))
+          (tags-todo "-SCHEDULED={.+}/+TODO")
+          (todo "DONE")
+          (todo "CANCELLED")))
 
         ("x" "Items that under going" todo "READY|BEGINED")
 
